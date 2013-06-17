3115a76fb005d636993e1b62c6edd7de09ea783d
Pavel Karneliuk <Pavel_Karneliuk@epam.com>
2013-06-17 20:32:44 +0000

    fix block allocator manager

--- tests/block_allocation_manager/main.cpp
+++ tests/block_allocation_manager/main.cpp
@@ -1,9 +1,16 @@
+//------------------------------------------------------------------------------
+// Author: Dzianis Huznou
+// Description: Test for BlockAllocatorManager.
+// Copyright (c) 2013 EPAM Systems. All Rights Reserved.
+//------------------------------------------------------------------------------
 #include <cassert>
 
+#include <iostream>
+
 #include "../../src/auxiliary/block_allocation_manager.h"
-
+//------------------------------------------------------------------------------
 using NST::auxiliary::BlockAllocatorManager;
-
+//------------------------------------------------------------------------------
 const int MIN_SIZE = 128;
 const int STEP_SIZE = 64;
 const int COUNT = 16;
@@ -11,32 +18,38 @@ const int BLOCK_SIZE = 128;
 const int BLOCK_LIMIT = 3;
 
 const int N_CHUNKS = BLOCK_SIZE * BLOCK_LIMIT;
+//------------------------------------------------------------------------------
 
 BlockAllocatorManager manager(MIN_SIZE, STEP_SIZE, COUNT, BLOCK_SIZE, BLOCK_LIMIT);
 void* ptrs[COUNT * BLOCK_SIZE * BLOCK_LIMIT] = {0};
-int t = 0;
 
-void allocate_all()
+int allocate_all()
 {
+    int n = 0;
     for(int i = 0; i < COUNT; ++i)
     {
         size_t s = MIN_SIZE + i * STEP_SIZE;
-        for(int j = 0; j < N_CHUNKS; ++j, ++t)
+        for(int j = 0; j < N_CHUNKS; ++j, ++n)
         {
-             ptrs[t] = manager.allocate(s);
-             assert(ptrs[t]!=NULL);
+             ptrs[n] = manager.allocate(s);
+             assert(ptrs[n]!=NULL);
         }
         assert(manager.allocate(s) == NULL);
     }
+    return n;
 }
+
 int main(int argc, char** argv)
 {
-    
-    allocate_all();
-    for(int i = 0; i < t; ++i)
+    int n = allocate_all();
+
+    std::cout << "end allocation: " << n <<  std::endl;
+    for(int i = 0; i < n; ++i)
     {
         manager.deallocate(ptrs[i]);
+        std::cout << "deallocate: " << i<<  std::endl;
     }
-    allocate_all();
+    n = allocate_all();
     return 0;
 }
+//------------------------------------------------------------------------------
