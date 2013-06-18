308241d8cf704b111c58fb5b08a9ef0ae478d4ae
Pavel Karneliuk <Pavel_Karneliuk@epam.com>
2013-06-18 14:56:59 +0000

    update RPC parsing and queueing data

--- src/filter/common/queueing_processor.h
+++ src/filter/common/queueing_processor.h
@@ -6,8 +6,11 @@
 #ifndef QUEUEING_PROCESSOR_H
 #define QUEUEING_PROCESSOR_H
 //------------------------------------------------------------------------------
+#include <algorithm> // for std::min()
 #include <memory> // for std::auto_ptr
 #include <string>
+
+#include <iostream>
 
 #include <pcap/pcap.h>
 
@@ -33,6 +36,12 @@ public:
 
     virtual void before_callback(pcap_t* handle)
     {
+        /*NFSData nfs;
+        
+        std::cout << "sizeof(NFSData): " << sizeof(nfs) << std::endl;
+        std::cout << "sizeof(NFSData.timestamp): " << sizeof(nfs.timestamp) << std::endl;
+        std::cout << "sizeof(NFSData.session.ip): " << sizeof(nfs.session.ip) << std::endl;
+        */
     }
 
     virtual void after_callback (pcap_t* handle)
@@ -48,6 +57,12 @@ public:
         NFSData nfs;
 
         nfs.timestamp = data.header->ts;
+        
+        nfs.rpc_len = std::min(data.rpc_length, sizeof(nfs.rpc_message));
+        memcpy(nfs.rpc_message, data.rpc_header, nfs.rpc_len);
+        
+        
+        
         // TODO:push data to queue
     }
 
