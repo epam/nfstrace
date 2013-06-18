b9a7483c474e2e3aa5b77d3e7306d3858373f165
Pavel Karneliuk <Pavel_Karneliuk@epam.com>
2013-06-18 12:33:09 +0000

    update fill_queue test

--- tests/fill_queue/fill_queue.cpp
+++ tests/fill_queue/fill_queue.cpp
@@ -3,6 +3,7 @@
 // Description: Test for NST:auxiliary::Queue<T>
 // Copyright (c) 2013 EPAM Systems. All Rights Reserved.
 //------------------------------------------------------------------------------
+#include <cassert>
 #include <iostream>
 
 #include "../../src/auxiliary/queue.h"
@@ -18,6 +19,8 @@ int main(int argc, char **argv)
 {
     Queue queue(10, 2); // two blocks of 10 elements
 
+    assert(queue.pop_list() == false); // empty queue
+
     // FILL QUEUE BY ELEMENTS
     std::cout << "fill elements:   ";
     for(unsigned int i=0; i<42; i++)    // try to push 42 elements
@@ -25,7 +28,7 @@ int main(int argc, char **argv)
         Data* data = queue.allocate();  // allocate element of data
         if(data == NULL)
         {
-            std::cout << "\nqueue is reached the limit of elements";
+            std::cout << "\nqueue has reached the limit of elements";
             break;
         }
         data->value = i;    // fill data
@@ -34,15 +37,18 @@ int main(int argc, char **argv)
     }
 
     // READ ELEMENTS
-    std::cout << "\nqueued elements: ";
     Queue::List list = queue.pop_list(); // take out list of all queued elements
-    while(list) // loop over all elements
+    assert(queue.pop_list() == false); // empty queue
+    std::cout << "\nqueued elements: ";
+    while(list) // loop over all taken elements
     {
-        Data* i = list.get();           // get first element
+        Data* i = list.get();   // get first element
         std::cout << i->value << " ";
-        queue.deallocate(i);            // free unused element
+        queue.deallocate(i);    // free unused element
     }
     std::cout << std::endl;
+
+    assert(queue.pop_list() == false); // empty queue
 
     return 0;
 }
