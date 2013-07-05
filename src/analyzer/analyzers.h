17ec728f9b8a0a39512545892e5e6bda0e19f8ea
Dzianis Huznou <Dzianis_Huznou@epam.com>
2013-07-05 13:50:52 +0000

    Interface unification

--- src/analyzer/analyzers.h
+++ src/analyzer/analyzers.h
@@ -30,30 +30,30 @@ class Analyzers
 public:
     Analyzers()
     {
-        methods.resize(22);
+        methods.resize(Proc::num);
 
-        methods[0] = &BaseAnalyzer::call_null;
-        methods[1] = &BaseAnalyzer::call_getattr;
-        //methods[2] = &BaseAnalyzer::call_setattr;
-        methods[3] = &BaseAnalyzer::call_lookup;
-        methods[4] = &BaseAnalyzer::call_access;
-        methods[5] = &BaseAnalyzer::call_readlink;
-        methods[6] = &BaseAnalyzer::call_read;
-        methods[7] = &BaseAnalyzer::call_write;
-        //methods[8] = &BaseAnalyzer::call_create;
-        //methods[9] = &BaseAnalyzer::call_mkdir;
-        //methods[10] = &BaseAnalyzer::call_symlink;
-        //methods[11] = &BaseAnalyzer::call_mknod;
-        methods[12] = &BaseAnalyzer::call_remove;
-        methods[13] = &BaseAnalyzer::call_rmdir;
-        methods[14] = &BaseAnalyzer::call_rename;
-        methods[15] = &BaseAnalyzer::call_link;
-        methods[16] = &BaseAnalyzer::call_readdir;
-        methods[17] = &BaseAnalyzer::call_readdirplus;
-        methods[18] = &BaseAnalyzer::call_fsstat;
-        methods[19] = &BaseAnalyzer::call_fsinfo;
-        methods[20] = &BaseAnalyzer::call_pathconf;
-        methods[21] = &BaseAnalyzer::call_commit;
+        methods[Proc::NFS_NULL]     = &BaseAnalyzer::call_null;
+        methods[Proc::GETATTR]      = &BaseAnalyzer::call_getattr;
+        methods[Proc::SETATTR]      = &BaseAnalyzer::call_setattr;
+        methods[Proc::LOOKUP]       = &BaseAnalyzer::call_lookup;
+        methods[Proc::ACCESS]       = &BaseAnalyzer::call_access;
+        methods[Proc::READLINK]     = &BaseAnalyzer::call_readlink;
+        methods[Proc::READ]         = &BaseAnalyzer::call_read;
+        methods[Proc::WRITE]        = &BaseAnalyzer::call_write;
+        methods[Proc::CREATE]       = &BaseAnalyzer::call_create;
+        methods[Proc::MKDIR]        = &BaseAnalyzer::call_mkdir;
+        methods[Proc::SYMLINK]      = &BaseAnalyzer::call_symlink;
+        methods[Proc::MKNOD]        = &BaseAnalyzer::call_mknod;
+        methods[Proc::REMOVE]       = &BaseAnalyzer::call_remove;
+        methods[Proc::RMDIR]        = &BaseAnalyzer::call_rmdir;
+        methods[Proc::RENAME]       = &BaseAnalyzer::call_rename;
+        methods[Proc::LINK]         = &BaseAnalyzer::call_link;
+        methods[Proc::READDIR]      = &BaseAnalyzer::call_readdir;
+        methods[Proc::READDIRPLUS]  = &BaseAnalyzer::call_readdirplus;
+        methods[Proc::FSSTAT]       = &BaseAnalyzer::call_fsstat;
+        methods[Proc::FSINFO]       = &BaseAnalyzer::call_fsinfo;
+        methods[Proc::PATHCONF]     = &BaseAnalyzer::call_pathconf;
+        methods[Proc::COMMIT]       = &BaseAnalyzer::call_commit;
     }
     ~Analyzers()
     {
@@ -70,56 +70,6 @@ public:
         analyzers.push_back(analyzer);
     }
 
-    bool call_setattr(const Session& session/*, const NFSOperation& operation*/)
-    {
-        Storage::iterator i = analyzers.begin();
-        Storage::iterator end = analyzers.end();
-        for(; i != end; ++i)
-        {
-            (*i)->call_setattr(session);
-        }
-        return true;
-    }
-    virtual bool call_create(const Session& session/*, const NFSOperation& operation*/)
-    {
-        Storage::iterator i = analyzers.begin();
-        Storage::iterator end = analyzers.end();
-        for(; i != end; ++i)
-        {
-            (*i)->call_create(session);
-        }
-        return true;
-    }
-    virtual bool call_mkdir(const Session& session/*, const NFSOperation& operation*/)
-    {
-        Storage::iterator i = analyzers.begin();
-        Storage::iterator end = analyzers.end();
-        for(; i != end; ++i)
-        {
-            (*i)->call_mkdir(session);
-        }
-        return true;
-    }
-    virtual bool call_symlink(const Session& session/*, const NFSOperation& operation*/)
-    {
-        Storage::iterator i = analyzers.begin();
-        Storage::iterator end = analyzers.end();
-        for(; i != end; ++i)
-        {
-            (*i)->call_symlink(session);
-        }
-        return true;
-    }
-    virtual bool call_mknod(const Session& session/*, const NFSOperation& operation*/)
-    {
-        Storage::iterator i = analyzers.begin();
-        Storage::iterator end = analyzers.end();
-        for(; i != end; ++i)
-        {
-            (*i)->call_mknod(session);
-        }
-        return true;
-    }
     virtual bool call(const Session& session, const NFSOperation& operation)
     {
         Storage::iterator i = analyzers.begin();
