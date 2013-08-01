91010629e340b35583b9d51f6edf861881e6a54a
Pavel Karneliuk <Pavel_Karneliuk@epam.com>
2013-08-01 14:48:51 +0000

    add coment to macro

--- src/auxiliary/logger.h
+++ src/auxiliary/logger.h
@@ -25,6 +25,7 @@
 #ifdef DEBUG
 #define STRINGIZE(x) DO_STRINGIZE(x)
 #define DO_STRINGIZE(x) #x
+// TODO: DANGEROUS MACRO !!! Passing custom client string as format to printf() may SIGSEGV
 #define TRACE(...) {\
     NST::auxiliary::Logger& log = NST::auxiliary::Logger::get_global();\
     log.print("\n" __FILE__ ":" STRINGIZE(__LINE__) ": " __VA_ARGS__);\
