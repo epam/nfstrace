04bbda035d2a5958739b3f37f728600badd00ec1
Pavel Karneliuk <Pavel_Karneliuk@epam.com>
2013-08-19 12:08:46 +0000

    Remove Conversation, add IPv4TCPMapper

--- src/filter/packet_info.h
+++ src/filter/packet_info.h
@@ -1,7 +1,7 @@
 //------------------------------------------------------------------------------
 // Author: Pavel Karneliuk
 // Description: Structure of pointers to captured pcap packet:
-//                 protocols, offset and known headers. WITHOUT data.
+//              protocols, offset and known headers. WITHOUT data.
 // Copyright (c) 2013 EPAM Systems. All Rights Reserved.
 //------------------------------------------------------------------------------
 #ifndef PACKET_INFO_H
@@ -14,12 +14,10 @@
 #include "ethernet/ethernet_header.h"
 #include "ip/ipv4_header.h"
 #include "tcp/tcp_header.h"
-#include "rpc/rpc_header.h"
 //------------------------------------------------------------------------------
 using namespace NST::filter::ethernet;
 using namespace NST::filter::ip;
 using namespace NST::filter::tcp;
-using namespace NST::filter::rpc;
 //------------------------------------------------------------------------------
 namespace NST
 {
@@ -28,78 +26,85 @@ namespace filter
 
 struct PacketInfo
 {
-    inline PacketInfo(const pcap_pkthdr* h, const uint8_t* p):header(h),packet(p)
+    inline PacketInfo(const pcap_pkthdr* h, const uint8_t* p, const uint32_t datalink):header(h),packet(p)
     {
         eth  = NULL;
         ipv4 = NULL;
         tcp  = NULL;
         data = packet;
         dlen = header->caplen;
+
+        switch(datalink)
+        {
+        case DLT_EN10MB:    check_eth(); break;
+        case DLT_LINUX_SLL: check_sll(); break;
+        }
     }
 
-    inline const EthernetHeader* check_eth()
+    inline void check_eth()
     {
-        if(dlen < sizeof(EthernetHeader)) return NULL;
-        eth = reinterpret_cast<const EthernetHeader*>(data);
+        if(dlen < sizeof(EthernetHeader)) return;
+        const EthernetHeader* header = reinterpret_cast<const EthernetHeader*>(data);
 
         data += sizeof(EthernetHeader);
         dlen -= sizeof(EthernetHeader);
 
-        switch(eth->type())
+        switch(header->type())
         {
         case ethernet_header::IP:   check_ipv4(); break;
         case ethernet_header::IPV6: // TODO: implement IPv6
         default:
-            return NULL;
+            return;
         }
 
-        return eth;
+        eth = header;
     }
 
-    inline const IPv4Header* check_ipv4()
+    inline void check_sll()
     {
-        if(dlen < sizeof(IPv4Header)) return NULL;
+    // TODO: add support Linux cooked sockets
+    }
+
+    inline void check_ipv4()
+    {
+        if(dlen < sizeof(IPv4Header)) return;
 
         const IPv4Header* header = reinterpret_cast<const IPv4Header*>(data);
 
-        if(header->version()  != 4) return NULL;
-        if(header->length() > dlen) return NULL; // fragmented payload
+        if(header->version()  != 4) return;
+        if(header->length() > dlen) return; // fragmented payload
 
         const uint32_t ihl = header->ihl();
-        if(ihl < 20 || ihl > 60) return NULL; // invalid IPv4  header length
-
-        ipv4 = header;
+        if(ihl < 20 || ihl > 60) return;    // invalid IPv4  header length
 
         data += ihl;
         dlen = header->length() - ihl;  // trunk data to length of IP packet
 
-        switch(ipv4->protocol())
+        switch(header->protocol())
         {
         case ipv4_header::TCP: check_tcp(); break;
         case ipv4_header::UDP: // TODO: implement UDP
         default:
-            return NULL;
+            return;
         }
 
-        return ipv4;
+        ipv4 = header;
     }
 
-    inline const TCPHeader* check_tcp()
+    inline void check_tcp()
     {
-        if(dlen < sizeof(TCPHeader)) return NULL;   // fragmented TCP header
+        if(dlen < sizeof(TCPHeader)) return;   // fragmented TCP header
 
         TCPHeader* header = (TCPHeader*)data;
         uint8_t offset = header->offset();
-        if(offset < 20 || offset > 60) return NULL; // invalid length of TCP header
+        if(offset < 20 || offset > 60) return; // invalid length of TCP header
 
-        if(dlen < offset) return NULL;
-
-        tcp = header;
+        if(dlen < offset) return;
 
         data += offset;
         dlen -= offset;
 
-        return tcp;
+        tcp = header;
     }
 
     // libpcap structures
