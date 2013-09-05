744ff32616ad6d9e8ee6f8c36dbea67f3e5d136c
Dzianis Huznou <Dzianis_Huznou@epam.com>
2013-09-05 13:28:20 +0000

    Disable variadic-macros warning

--- config.mk
+++ config.mk
@@ -2,7 +2,7 @@ TARGET:=nfstrace
 CC:=g++
 
 LIBS   += -lstdc++ -lpthread -lpcap
-CFLAGS += -static-libgcc -Wall
+CFLAGS += -static-libgcc -Wall -pedantic -Wno-variadic-macros
 
 RELEASE_FLAGS += -O3 -DNDEBUG
 DEBUG_FLAGS   += -O0 -g -DDEBUG
