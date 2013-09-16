62e3fb100f495b178aeaab30313fa1e3923505a0
Pavel Karneliuk <Pavel_Karneliuk@epam.com>
2013-09-16 11:30:31 +0000

    add conditional linking with -ldl on Linux

--- config.mk
+++ config.mk
@@ -1,8 +1,13 @@
 TARGET:=nfstrace
 CC:=g++
+OS:=$(shell uname -s)
 
-LIBS   += -lstdc++ -lpthread -lpcap -ldl
+LIBS   += -lstdc++ -lpthread -lpcap
 CFLAGS += -static-libgcc -Wall -pedantic -Wno-variadic-macros -Wno-long-long
+
+ifeq ($(OS),Linux)
+    LIBS += -ldl # dynamic linking loader on GNU/Linux
+endif
 
 RELEASE_FLAGS += -O3 -DNDEBUG
 DEBUG_FLAGS   += -O0 -g -DDEBUG
