ea461d193edd86669143aed3e88fb28b53086b00
Pavel Karneliuk <Pavel_Karneliuk@epam.com>
2013-09-25 16:35:30 +0000

    fix path to application in usage samples

--- scripts/usage-samples/show-usage.sh
+++ scripts/usage-samples/show-usage.sh
@@ -1,11 +1,11 @@
 #/bin/sh
 
-NST=../release/nfstrace
-MODULES=../analyzers/release
+APP=../../release/nfstrace
+MODULES=../../analyzers/release
 
 echo 'This script demonstrates avaliable options for application and pluggable analysis modules'
 
-$NST --help                                 \
+$APP --help                                 \
      --analyzer=$MODULES/libbreakdown.so    \
      --analyzer=$MODULES/libofws.so         \
      --analyzer=$MODULES/libofdws.so
