08a74d4f33987bf84cb3b30e767ab5c38f653c24
Pavel Karneliuk <Pavel_Karneliuk@epam.com>
2013-10-04 05:20:20 +0000

    Disable

--- src/filter/filtration_manager.cpp
+++ src/filter/filtration_manager.cpp
@@ -8,13 +8,11 @@
 #include "common/dumping.h"
 #include "common/queueing.h"
 #include "filtration_manager.h"
-#include "pcap/async_capture_reader.h"
 #include "pcap/capture_reader.h"
 #include "pcap/file_reader.h"
 #include "processing_thread.h"
 //------------------------------------------------------------------------------
 using NST::auxiliary::UniquePtr;
-using NST::filter::pcap::AsyncCaptureReader;
 using NST::filter::pcap::CaptureReader;
 using NST::filter::pcap::FileReader;
 //------------------------------------------------------------------------------
@@ -23,95 +21,57 @@ namespace NST
 namespace filter
 {
 
-template <typename CaptureReaderType>
-static UniquePtr<CaptureReaderType> create_capture_reader(const Parameters& params)
+static UniquePtr<CaptureReader> create_capture_reader(const Parameters& params)
 {
     const int read_timeout = 250; // milliseconds
-    UniquePtr<CaptureReaderType> reader (new CaptureReaderType(
-                                            params.interface(),
-                                            params.filter(),
-                                            params.snaplen(),
-                                            read_timeout,
-                                            params.buffer_size()
-                                            )
-                                        );
+    UniquePtr<CaptureReader> reader (new CaptureReader(params.interface(),
+                                                       params.filter(),
+                                                       params.snaplen(),
+                                                       read_timeout,
+                                                       params.buffer_size()
+                                                       ));
     return reader;
 }
 
 FiltrationManager::FiltrationManager(RunningStatus& s, const Parameters& params) : status(s)
 {
-    if(params.tmp_buffering())
-    {
-        typedef FiltrationProcessor<AsyncCaptureReader, Dumping> AsyncProcessor;
-        typedef ProcessingThread<AsyncProcessor> OnlineDumping;
+    typedef FiltrationProcessor<CaptureReader, Dumping> Processor;
+    typedef ProcessingThread<Processor> OnlineDumping;
 
-        UniquePtr<AsyncCaptureReader> reader = create_capture_reader<AsyncCaptureReader>(params);
-        UniquePtr<Dumping>            writer (new Dumping(reader->get_handle(),
-                                                     params.output_file(),
-                                                     params.dumping_cmd(),
-                                                     params.dumping_size()));
+    UniquePtr<CaptureReader> reader = create_capture_reader(params);
+    UniquePtr<Dumping>       writer (new Dumping(reader->get_handle(),
+                                                 params.output_file(),
+                                                 params.dumping_cmd(),
+                                                 params.dumping_size()));
 
-        UniquePtr<AsyncProcessor>     processor (new AsyncProcessor(reader, writer));
-        UniquePtr<Thread> thread      (new OnlineDumping(processor, status));
+    UniquePtr<Processor>     processor (new Processor(reader, writer));
+    UniquePtr<Thread> thread (new OnlineDumping(processor, status));
 
-        threads.add(thread);
-    }
-    else
-    {
-        typedef FiltrationProcessor<CaptureReader, Dumping> Processor;
-        typedef ProcessingThread<Processor> OnlineDumping;
-
-        UniquePtr<CaptureReader> reader = create_capture_reader<CaptureReader>(params);
-        UniquePtr<Dumping>       writer (new Dumping(reader->get_handle(),
-                                                     params.output_file(),
-                                                     params.dumping_cmd(),
-                                                     params.dumping_size()));
-
-        UniquePtr<Processor>     processor (new Processor(reader, writer));
-        UniquePtr<Thread> thread    (new OnlineDumping(processor, status));
-
-        threads.add(thread);
-    }
+    threads.add(thread);
 }
 FiltrationManager::FiltrationManager(RunningStatus& s, FilteredDataQueue& queue, const Parameters& params) : status(s)
 {
-    if(params.tmp_buffering())
-    {
-        typedef FiltrationProcessor<AsyncCaptureReader, Queueing> AsyncProcessor;
-        typedef ProcessingThread<AsyncProcessor> OnlineAnalyzing;
+    typedef FiltrationProcessor<CaptureReader, Queueing> Processor;
+    typedef ProcessingThread<Processor> OnlineAnalyzing;
 
-        UniquePtr<AsyncCaptureReader>   reader = create_capture_reader<AsyncCaptureReader>(params);
-        UniquePtr<Queueing>             writer (new Queueing(queue));
+    UniquePtr<CaptureReader> reader = create_capture_reader(params);
+    UniquePtr<Queueing>      writer (new Queueing(queue));
 
-        UniquePtr<AsyncProcessor>       processor (new AsyncProcessor(reader, writer));
-        UniquePtr<Thread> thread        (new OnlineAnalyzing(processor, status));
+    UniquePtr<Processor>     processor (new Processor(reader, writer));
+    UniquePtr<Thread> thread (new OnlineAnalyzing(processor, status));
 
-        threads.add(thread);
-    }
-    else
-    {
-        typedef FiltrationProcessor<CaptureReader, Queueing> Processor;
-        typedef ProcessingThread<Processor> OnlineAnalyzing;
-
-        UniquePtr<CaptureReader>   reader = create_capture_reader<CaptureReader>(params);
-        UniquePtr<Queueing>        writer (new Queueing(queue));
-
-        UniquePtr<Processor>       processor (new Processor(reader, writer));
-        UniquePtr<Thread> thread   (new OnlineAnalyzing(processor, status));
-
-        threads.add(thread);
-    }
+    threads.add(thread);
 }
 FiltrationManager::FiltrationManager(RunningStatus& s, FilteredDataQueue& queue, const std::string& ifile) : status(s)
 {
     typedef FiltrationProcessor<FileReader, Queueing> Processor;
     typedef ProcessingThread<Processor> OfflineAnalyzing;
 
-    UniquePtr<FileReader> reader (new FileReader(ifile));
-    UniquePtr<Queueing>   writer (new Queueing(queue));
+    UniquePtr<FileReader>    reader (new FileReader(ifile));
+    UniquePtr<Queueing>      writer (new Queueing(queue));
 
-    UniquePtr<Processor>        processor (new Processor(reader, writer));
-    UniquePtr<Thread> thread    (new OfflineAnalyzing(processor, status));
+    UniquePtr<Processor>     processor (new Processor(reader, writer));
+    UniquePtr<Thread> thread (new OfflineAnalyzing(processor, status));
 
     threads.add(thread);
 }
