90c4135c8002a2f7ea7ac8ab77f0eafc8d6a0759
Pavel Karneliuk <pavel_karneliuk@epam.com>
2014-03-13 14:40:38 +0300

    add (-T)--trace option for print out RPC procedures, it is set by default if no module was passed(-a)

--- src/controller/cmdline_args.cpp
+++ src/controller/cmdline_args.cpp
@@ -26,17 +26,18 @@ Opt Args::options[Args::num] =
     {'i', "interface",  Opt::REQ, "",                    "listen interface, it is required for " LIVE " and " DUMP " modes", "INTERFACE",      nullptr, false},
     {'f', "filtration", Opt::REQ, "ip and port 2049",    "a packet filtration in libpcap BPF syntax",                        "BPF",            nullptr, false},
     {'s', "snaplen",    Opt::REQ, "65535",               "max length of raw captured packet. May be used ONLY FOR UDP",      "0..65535",       nullptr, false},
-    {'b', "bsize",      Opt::REQ, "20",                  "size of capturing kernel buffer",                                  "MBytes",         nullptr, false},
     {'t', "timeout",    Opt::REQ, "100",                 "set the read timeout that will be used on a capture",              "Milliseconds",   nullptr, false},
+    {'b', "bsize",      Opt::REQ, "20",                  "size of capturing kernel buffer",                                  "MBytes",         nullptr, false},
     {'m', "mode",       Opt::REQ, LIVE,                  "set runing mode",                                          LIVE "|" DUMP "|" STAT,   nullptr, false},
-    {'a', "analysis",   Opt::MUL, "",                    "specify path to analysis module and pass desired options", "PATH#opt1,opt2=val,...", nullptr, false},
+    {'a', "analysis",   Opt::MUL, "",                    "specify path to analysis module and set desired options",  "PATH#opt1,opt2=val,...", nullptr, false},
     {'I', "ifile",      Opt::REQ, "INTERFACE-BPF.pcap",  "input file for " STAT " mode, the '-' means stdin",        "PATH",   nullptr, false},
     {'O', "ofile",      Opt::REQ, "INTERFACE-BPF.pcap",  "output file for " DUMP " mode, the '-' means stdout",      "PATH",   nullptr, false},
     {'C', "command",    Opt::REQ, "",                    "execute command for each dumped file",                     nullptr,  nullptr, false},
     {'D', "dump-size",  Opt::REQ, "0",                   "size of dumping file portion, 0 = no limit",               "MBytes", nullptr, false},
     {'M', "msg-header", Opt::REQ, "512",                 "RPC message will be truncated to this limit in bytes before passing to Analysis", "1..4000", nullptr, false},
     {'Q', "qcapacity",  Opt::REQ, "4096",                "initial queue capacity of RPC messages",                                         "1..65535", nullptr, false},
-    {'v', "verbose",    Opt::NOA, "false",               "print out additional information and trace to console collected NFSv3 procedures", nullptr, nullptr, false},
+    {'T', "trace",      Opt::NOA, "false",               "print collected NFSv3 procedures, true if no modules were passed(by -a)",        nullptr, nullptr, false},
+    {'v', "verbose",    Opt::NOA, "false",               "print out additional information",                                               nullptr, nullptr, false},
     {'h', "help",       Opt::NOA, "false",               "print this help message and usage for modules passed via -a options, then exit", nullptr, nullptr, false}
 };
 
