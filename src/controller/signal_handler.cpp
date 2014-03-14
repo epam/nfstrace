d96a4149df22809789346c073f7cac7b36d3e36a
Pavel Karneliuk <pavel_karneliuk@epam.com>
2014-03-14 14:30:34 +0300

    fix defect in handling SIGCHLD on FreeBSD

--- src/controller/signal_handler.cpp
+++ src/controller/signal_handler.cpp
@@ -3,8 +3,7 @@
 // Description: Handling signals and map them to exceptions.
 // Copyright (c) 2013 EPAM Systems. All Rights Reserved.
 //------------------------------------------------------------------------------
-#include <iostream>
-#include <functional>
+#include <functional>   // std::ref
 
 #include <unistd.h>
 #include <pthread.h>
@@ -25,22 +24,14 @@ SignalHandler::Signal::Signal(int sig) : std::runtime_error(::strsignal(sig))
 {
 }
 
-static void handle_signals(std::atomic_flag& running, RunningStatus& status)
+static void dummy(int) {}
+
+static void handle_signals(const sigset_t /*mask*/, std::atomic_flag& running, RunningStatus& status)
 {
-    sigset_t mask;
-    ::sigfillset(&mask);
-    ::pthread_sigmask(SIG_BLOCK, &mask, NULL);
-
-    ::sigemptyset(&mask);
-    ::sigaddset(&mask, SIGINT);
-    ::sigaddset(&mask, SIGQUIT);
-    ::sigaddset(&mask, SIGCHLD);
-
-    int signo = 0;
-
     while(running.test_and_set())
     {
-        ::sigwait(&mask, &signo);   // synchronously wait of the signals
+        int signo = 0;
+        ::sigwait(&mask, &signo);  // synchronously wait signals
 
         if(signo == SIGCHLD)
         {
@@ -58,18 +49,26 @@ SignalHandler::SignalHandler(RunningStatus& s)
 : handler{}
 , running{ATOMIC_FLAG_INIT} // false
 {
+    // set dummy handler for SIGCHLD to prevent ignoring it on FreeBSD
+    struct sigaction chld;
+    memset(&chld, 0, sizeof(chld));
+    chld.sa_handler = dummy;
+    ::sigaction(SIGCHLD, &chld, NULL);
+
     sigset_t mask;
-    ::sigfillset(&mask);
+    ::sigemptyset(&mask);
+    ::sigaddset(&mask, SIGINT);  // correct exit from program by Ctrl-C
+    ::sigaddset(&mask, SIGCHLD); // stop sigwait-thread and wait children
     ::pthread_sigmask(SIG_BLOCK, &mask, NULL);
 
     running.test_and_set();
-    handler = std::thread{handle_signals, std::ref(running), std::ref(s)};
+    handler = std::thread{handle_signals, mask, std::ref(running), std::ref(s)};
 }
 SignalHandler::~SignalHandler()
 {
     running.clear();
-    // send signal ourself to stop thread execution via unblock sigwait()
-    ::kill(::getpid(), SIGCHLD);
+    // send signal to stop handler thread execution via unblock sigwait()
+    ::pthread_kill(handler.native_handle(), SIGCHLD);
     handler.join();
 }
 
