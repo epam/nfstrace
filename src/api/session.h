246ee177342f2f6d66f738cf578a2dadbcef3475
Pavel Karneliuk <pavel_karneliuk@epam.com>
2014-04-07 20:47:51 +0300

    add naive IPv6 support; replace host BO with network BO in addresses and ports

--- src/api/session.h
+++ src/api/session.h
@@ -7,6 +7,8 @@
 #define SESSION_H
 //------------------------------------------------------------------------------
 #include <cstdint>
+
+#include <netinet/in.h> // for in_port_t, in_addr_t types
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 namespace NST
@@ -23,29 +25,29 @@ struct Session
         Unknown     =0xBAD
     };
 
-    enum IPType
-    {
-        v4=0,
-        v6=1
-    } ip_type:16;       // 16 bit for alignment following integers
-
     enum Type
     {
         TCP=0,
         UDP=1
     } type   :16;       // 16 bit for alignment following integers
 
-    uint16_t port[2];   // 2 ports in host byte order
+    enum IPType
+    {
+        v4=0,
+        v6=1
+    } ip_type:16;       // 16 bit for alignment following integers
+
+    in_port_t port[2];   // 2 ports in network byte order
 
     union IPAddress
     {
         struct
         {
-            uint32_t addr[2];   // 2 IPv4 addresses in host byte order
+            in_addr_t addr[2];    // 2 IPv4 addresses in network byte order
         } v4;
         struct
         {
-            uint8_t addr[2][16];// 2 IPv6 addresses in host byte order
+            uint8_t addr[2][16];   // 2 IPv6 addresses in network byte order
         } v6;
     } ip;
 };
