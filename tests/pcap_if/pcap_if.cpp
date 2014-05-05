d69b3a703a02561ec6073aafdde135f61e65d2aa
Alexey Costroma <alexey_costroma@epam.com>
2014-05-05 16:18:28 +0300

    Merge branch 'if-list' into 'master'

--- tests/pcap_if/pcap_if.cpp
+++ tests/pcap_if/pcap_if.cpp
@@ -14,14 +14,17 @@ using namespace NST::filtration::pcap;
 int main()
 {
     std::cout << "Note: Reading list of network interfaces may "
-                  "require that you have special privileges." << std::endl;
+                  "require that you have special privileges." << std::endl << std::endl;
+
     try
     {
         NetworkInterfaces interfaces;
-        for(auto i : interfaces)
+        for(auto& i : interfaces)
         {
             std::cout << i << '\n';
-        }
+            for(auto a : i) std::cout << '\t' << a << '\n';
+        } 
+        std::cout << "\n[default]: " <<  interfaces.default_device() << "\n\n";
     }
     catch(const PcapError& e)
     {
