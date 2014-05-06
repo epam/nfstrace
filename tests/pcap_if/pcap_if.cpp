b4ed418e27cda02aee366a6f19095b5c86e8b987
Pavel Karneliuk <pavel_karneliuk@epam.com>
2014-05-06 14:35:53 +0300

    Squashed commit of the following:
    commit 36c824ee6cd1815ba10669e9ab1878fc24766bec
    Author: Pavel Karneliuk <pavel_karneliuk@epam.com>
    Date:   Tue May 6 14:31:31 2014 +0300
    
        fix error handling in case of insufficient privileges for libpcap

--- tests/pcap_if/pcap_if.cpp
+++ tests/pcap_if/pcap_if.cpp
@@ -14,12 +14,12 @@ using namespace NST::filtration::pcap;
 int main()
 {
     std::cout << "Note: Reading list of network interfaces may "
-                  "require that you have special privileges." << std::endl << std::endl;
+                  "require that you have special privileges." << std::endl;
 
     try
     {
         NetworkInterfaces interfaces;
-        for(auto& i : interfaces)
+        for(auto i : interfaces)
         {
             std::cout << i << '\n';
             for(auto a : i) std::cout << '\t' << a << '\n';
