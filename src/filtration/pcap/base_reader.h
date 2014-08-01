1f4fd8ceaeb16ccb81c64d72ab31f891bcadc01e
Mikhail Litvinets <mikhail_litvinets@epam.com>
2014-08-01 11:14:30 +0300

    typo

--- src/filtration/pcap/base_reader.h
+++ src/filtration/pcap/base_reader.h
@@ -1,6 +1,6 @@
 //------------------------------------------------------------------------------
 // Author: Pavel Karneliuk (Dzianis Huznou)
-// Description: High level interface for passing info Processor.
+// Description: High level interface for passing raw frames into Processor.
 // Copyright (c) 2013 EPAM Systems
 //------------------------------------------------------------------------------
 /*
