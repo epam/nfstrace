66796c8ff4ac8a61ee5385fa0a1744cb54807603
Andrey Kuznetsov <Andrey_Kuznetsov@epam.com>
2014-11-28 10:08:02 +0300

    Code style has been checked

--- src/api/ianalyzer_type.h
+++ src/api/ianalyzer_type.h
@@ -39,82 +39,82 @@ class INFSv3rpcgen
 {
 public:
     virtual void null(const struct RPCProcedure*,
-            const struct rpcgen::NULL3args*,
-            const struct rpcgen::NULL3res*) {}
+                      const struct rpcgen::NULL3args*,
+                      const struct rpcgen::NULL3res*) {}
     virtual void getattr3(const struct RPCProcedure*,
-            const struct rpcgen::GETATTR3args*,
-            const struct rpcgen::GETATTR3res*) {}
+                          const struct rpcgen::GETATTR3args*,
+                          const struct rpcgen::GETATTR3res*) {}
     virtual void setattr3(const struct RPCProcedure*,
-            const struct rpcgen::SETATTR3args*,
-            const struct rpcgen::SETATTR3res*) {}
+                          const struct rpcgen::SETATTR3args*,
+                          const struct rpcgen::SETATTR3res*) {}
     virtual void lookup3(const struct RPCProcedure*,
-            const struct rpcgen::LOOKUP3args*,
-            const struct rpcgen::LOOKUP3res*) {}
+                         const struct rpcgen::LOOKUP3args*,
+                         const struct rpcgen::LOOKUP3res*) {}
     virtual void access3(const struct RPCProcedure*,
-            const struct rpcgen::ACCESS3args*,
-            const struct rpcgen::ACCESS3res*) {}
+                         const struct rpcgen::ACCESS3args*,
+                         const struct rpcgen::ACCESS3res*) {}
     virtual void readlink3(const struct RPCProcedure*,
-            const struct rpcgen::READLINK3args*,
-            const struct rpcgen::READLINK3res*) {}
+                           const struct rpcgen::READLINK3args*,
+                           const struct rpcgen::READLINK3res*) {}
     virtual void read3(const struct RPCProcedure*,
-            const struct rpcgen::READ3args*,
-            const struct rpcgen::READ3res*) {}
+                       const struct rpcgen::READ3args*,
+                       const struct rpcgen::READ3res*) {}
     virtual void write3(const struct RPCProcedure*,
-            const struct rpcgen::WRITE3args*,
-            const struct rpcgen::WRITE3res*) {}
+                        const struct rpcgen::WRITE3args*,
+                        const struct rpcgen::WRITE3res*) {}
     virtual void create3(const struct RPCProcedure*,
-            const struct rpcgen::CREATE3args*,
-            const struct rpcgen::CREATE3res*) {}
+                         const struct rpcgen::CREATE3args*,
+                         const struct rpcgen::CREATE3res*) {}
     virtual void mkdir3(const struct RPCProcedure*,
-            const struct rpcgen::MKDIR3args*,
-            const struct rpcgen::MKDIR3res*) {}
+                        const struct rpcgen::MKDIR3args*,
+                        const struct rpcgen::MKDIR3res*) {}
     virtual void symlink3(const struct RPCProcedure*,
-            const struct rpcgen::SYMLINK3args*,
-            const struct rpcgen::SYMLINK3res*) {}
+                          const struct rpcgen::SYMLINK3args*,
+                          const struct rpcgen::SYMLINK3res*) {}
     virtual void mknod3(const struct RPCProcedure*,
-            const struct rpcgen::MKNOD3args*,
-            const struct rpcgen::MKNOD3res*) {}
+                        const struct rpcgen::MKNOD3args*,
+                        const struct rpcgen::MKNOD3res*) {}
     virtual void remove3(const struct RPCProcedure*,
-            const struct rpcgen::REMOVE3args*,
-            const struct rpcgen::REMOVE3res*) {}
+                         const struct rpcgen::REMOVE3args*,
+                         const struct rpcgen::REMOVE3res*) {}
     virtual void rmdir3(const struct RPCProcedure*,
-            const struct rpcgen::RMDIR3args*,
-            const struct rpcgen::RMDIR3res*) {}
+                        const struct rpcgen::RMDIR3args*,
+                        const struct rpcgen::RMDIR3res*) {}
     virtual void rename3(const struct RPCProcedure*,
-            const struct rpcgen::RENAME3args*,
-            const struct rpcgen::RENAME3res*) {}
+                         const struct rpcgen::RENAME3args*,
+                         const struct rpcgen::RENAME3res*) {}
     virtual void link3(const struct RPCProcedure*,
-            const struct rpcgen::LINK3args*,
-            const struct rpcgen::LINK3res*) {}
+                       const struct rpcgen::LINK3args*,
+                       const struct rpcgen::LINK3res*) {}
     virtual void readdir3(const struct RPCProcedure*,
-            const struct rpcgen::READDIR3args*,
-            const struct rpcgen::READDIR3res*) {}
+                          const struct rpcgen::READDIR3args*,
+                          const struct rpcgen::READDIR3res*) {}
     virtual void readdirplus3(const struct RPCProcedure*,
-            const struct rpcgen::READDIRPLUS3args*,
-            const struct rpcgen::READDIRPLUS3res*) {}
+                              const struct rpcgen::READDIRPLUS3args*,
+                              const struct rpcgen::READDIRPLUS3res*) {}
     virtual void fsstat3(const struct RPCProcedure*,
-            const struct rpcgen::FSSTAT3args*,
-            const struct rpcgen::FSSTAT3res*) {}
+                         const struct rpcgen::FSSTAT3args*,
+                         const struct rpcgen::FSSTAT3res*) {}
     virtual void fsinfo3(const struct RPCProcedure*,
-            const struct rpcgen::FSINFO3args*,
-            const struct rpcgen::FSINFO3res*) {}
+                         const struct rpcgen::FSINFO3args*,
+                         const struct rpcgen::FSINFO3res*) {}
     virtual void pathconf3(const struct RPCProcedure*,
-            const struct rpcgen::PATHCONF3args*,
-            const struct rpcgen::PATHCONF3res*) {}
+                           const struct rpcgen::PATHCONF3args*,
+                           const struct rpcgen::PATHCONF3res*) {}
     virtual void commit3(const struct RPCProcedure*,
-            const struct rpcgen::COMMIT3args*,
-            const struct rpcgen::COMMIT3res*) {}
+                         const struct rpcgen::COMMIT3args*,
+                         const struct rpcgen::COMMIT3res*) {}
 };
 
 class INFSv4rpcgen
 {
 public:
     virtual void null(const struct RPCProcedure*,
-            const struct rpcgen::NULL4args*,
-            const struct rpcgen::NULL4res*) {}
+                      const struct rpcgen::NULL4args*,
+                      const struct rpcgen::NULL4res*) {}
     virtual void compound4(const struct RPCProcedure*,
-            const struct rpcgen::COMPOUND4args*,
-            const struct rpcgen::COMPOUND4res*) {}
+                           const struct rpcgen::COMPOUND4args*,
+                           const struct rpcgen::COMPOUND4res*) {}
 };
 
 /*! Abstract interface of plugin which collects SMBv1 statistic
@@ -659,133 +659,133 @@ public:
      * \param arg - arguments for the command
      * \param res - result of the command
      */
-    virtual void closeFileSMBv2(const SMBv2::CloseFileCommand *, const SMBv2::CloseFileArgumentType &, const SMBv2::CloseFileResultType &) {}
+    virtual void closeFileSMBv2(const SMBv2::CloseFileCommand*, const SMBv2::CloseFileArgumentType&, const SMBv2::CloseFileResultType&) {}
 
     /*! "Negotiate" command "on receive" event handler
      * \param cmd - Specified command
      * \param arg - arguments for the command
      * \param res - result of the command
      */
-    virtual void negotiateSMBv2(const SMBv2::NegotiateCommand *, const SMBv2::NegotiateArgumentType &, const SMBv2::NegotiateResultType &) {}
+    virtual void negotiateSMBv2(const SMBv2::NegotiateCommand*, const SMBv2::NegotiateArgumentType&, const SMBv2::NegotiateResultType&) {}
 
     /*! "session setup" command "on receive" event handler
      * \param cmd - Specified command
      * \param arg - arguments for the command
      * \param res - result of the command
      */
-    virtual void sessionSetupSMBv2(const SMBv2::SessionSetupCommand *, const SMBv2::SessionSetupArgumentType &, const SMBv2::SessionSetupResultType &) {}
+    virtual void sessionSetupSMBv2(const SMBv2::SessionSetupCommand*, const SMBv2::SessionSetupArgumentType&, const SMBv2::SessionSetupResultType&) {}
 
     /*! "log off" command "on receive" event handler
      * \param cmd - Specified command
      * \param arg - arguments for the command
      * \param res - result of the command
      */
-    virtual void logOffSMBv2(const SMBv2::LogOffCommand *, const SMBv2::LogOffArgumentType &, const SMBv2::LogOffResultType &) {}
+    virtual void logOffSMBv2(const SMBv2::LogOffCommand*, const SMBv2::LogOffArgumentType&, const SMBv2::LogOffResultType&) {}
 
     /*! "Tree Connect" command "on receive" event handler
      * \param cmd - Specified command
      * \param arg - arguments for the command
      * \param res - result of the command
      */
-    virtual void treeConnectSMBv2(const SMBv2::TreeConnectCommand *, const SMBv2::TreeConnectArgumentType &, const SMBv2::TreeConnectResultType &) {}
+    virtual void treeConnectSMBv2(const SMBv2::TreeConnectCommand*, const SMBv2::TreeConnectArgumentType&, const SMBv2::TreeConnectResultType&) {}
 
     /*! "Tree disconnect" command "on receive" event handler
      * \param cmd - Specified command
      * \param arg - arguments for the command
      * \param res - result of the command
      */
-    virtual void treeDisconnectSMBv2(const SMBv2::TreeDisconnectCommand *, const SMBv2::TreeDisconnectArgumentType &, const SMBv2::TreeDisconnectResultType &) {}
+    virtual void treeDisconnectSMBv2(const SMBv2::TreeDisconnectCommand*, const SMBv2::TreeDisconnectArgumentType&, const SMBv2::TreeDisconnectResultType&) {}
 
     /*! "Create" command "on receive" event handler
      * \param cmd - Specified command
      * \param arg - arguments for the command
      * \param res - result of the command
      */
-    virtual void createSMBv2(const SMBv2::CreateCommand *, const SMBv2::CreateArgumentType &, const SMBv2::CreateResultType &) {}
+    virtual void createSMBv2(const SMBv2::CreateCommand*, const SMBv2::CreateArgumentType&, const SMBv2::CreateResultType&) {}
 
     /*! "Flush" command "on receive" event handler
      * \param cmd - Specified command
      * \param arg - arguments for the command
      * \param res - result of the command
      */
-    virtual void flushSMBv2(const SMBv2::FlushCommand *, const SMBv2::FlushArgumentType &, const SMBv2::FlushResultType &) {}
+    virtual void flushSMBv2(const SMBv2::FlushCommand*, const SMBv2::FlushArgumentType&, const SMBv2::FlushResultType&) {}
 
     /*! "Read" command "on receive" event handler
      * \param cmd - Specified command
      * \param arg - arguments for the command
      * \param res - result of the command
      */
-    virtual void readSMBv2(const SMBv2::ReadCommand *, const SMBv2::ReadArgumentType &, const SMBv2::ReadResultType &) {}
+    virtual void readSMBv2(const SMBv2::ReadCommand*, const SMBv2::ReadArgumentType&, const SMBv2::ReadResultType&) {}
 
     /*! "Write" command "on receive" event handler
      * \param cmd - Specified command
      * \param arg - arguments for the command
      * \param res - result of the command
      */
-    virtual void writeSMBv2(const SMBv2::WriteCommand *, const SMBv2::WriteArgumentType &, const SMBv2::WriteResultType &) {}
+    virtual void writeSMBv2(const SMBv2::WriteCommand*, const SMBv2::WriteArgumentType&, const SMBv2::WriteResultType&) {}
 
     /*! "Lock" command "on receive" event handler
      * \param cmd - Specified command
      * \param arg - arguments for the command
      * \param res - result of the command
      */
-    virtual void lockSMBv2(const SMBv2::LockCommand *, const SMBv2::LockArgumentType &, const SMBv2::LockResultType &) {}
+    virtual void lockSMBv2(const SMBv2::LockCommand*, const SMBv2::LockArgumentType&, const SMBv2::LockResultType&) {}
 
     /*! "IO ctl" command "on receive" event handler
      * \param cmd - Specified command
      * \param arg - arguments for the command
      * \param res - result of the command
      */
-    virtual void ioctlSMBv2(const SMBv2::IoctlCommand *, const SMBv2::IoctlArgumentType &, const SMBv2::IoctlResultType &) {}
+    virtual void ioctlSMBv2(const SMBv2::IoctlCommand*, const SMBv2::IoctlArgumentType&, const SMBv2::IoctlResultType&) {}
 
     /*! "Cancel" command "on receive" event handler
      * \param cmd - Specified command
      * \param arg - arguments for the command
      * \param res - result of the command
      */
-    virtual void cancelSMBv2(const SMBv2::CancelCommand *, const SMBv2::CancelArgumentType &, const SMBv2::CancelResultType &) {}
+    virtual void cancelSMBv2(const SMBv2::CancelCommand*, const SMBv2::CancelArgumentType&, const SMBv2::CancelResultType&) {}
 
     /*! "Echo" command "on receive" event handler
      * \param cmd - Specified command
      * \param arg - arguments for the command
      * \param res - result of the command
      */
-    virtual void echoSMBv2(const SMBv2::EchoCommand *, const SMBv2::EchoArgumentType &, const SMBv2::EchoResultType &) {}
+    virtual void echoSMBv2(const SMBv2::EchoCommand*, const SMBv2::EchoArgumentType&, const SMBv2::EchoResultType&) {}
 
     /*! "Query directory" command "on receive" event handler
      * \param cmd - Specified command
      * \param arg - arguments for the command
      * \param res - result of the command
      */
-    virtual void queryDirSMBv2(const SMBv2::QueryDirCommand *, const SMBv2::QueryDirArgumentType &, const SMBv2::QueryDirResultType &) {}
+    virtual void queryDirSMBv2(const SMBv2::QueryDirCommand*, const SMBv2::QueryDirArgumentType&, const SMBv2::QueryDirResultType&) {}
 
     /*! "Change notify" command "on receive" event handler
      * \param cmd - Specified command
      * \param arg - arguments for the command
      * \param res - result of the command
      */
-    virtual void changeNotifySMBv2(const SMBv2::ChangeNotifyCommand *, const SMBv2::ChangeNotifyArgumentType &, const SMBv2::ChangeNotifyResultType &) {}
+    virtual void changeNotifySMBv2(const SMBv2::ChangeNotifyCommand*, const SMBv2::ChangeNotifyArgumentType&, const SMBv2::ChangeNotifyResultType&) {}
 
     /*! "Query Info" command "on receive" event handler
      * \param cmd - Specified command
      * \param arg - arguments for the command
      * \param res - result of the command
      */
-    virtual void queryInfoSMBv2(const SMBv2::QueryInfoCommand *, const SMBv2::QueryInfoArgumentType &, const SMBv2::QueryInfoResultType &) {}
+    virtual void queryInfoSMBv2(const SMBv2::QueryInfoCommand*, const SMBv2::QueryInfoArgumentType&, const SMBv2::QueryInfoResultType&) {}
 
     /*! "Set Info" command "on receive" event handler
      * \param cmd - Specified command
      * \param arg - arguments for the command
      * \param res - result of the command
      */
-    virtual void setInfoSMBv2(const SMBv2::SetInfoCommand *, const SMBv2::SetInfoArgumentType &, const SMBv2::SetInfoResultType &) {}
+    virtual void setInfoSMBv2(const SMBv2::SetInfoCommand*, const SMBv2::SetInfoArgumentType&, const SMBv2::SetInfoResultType&) {}
 
     /*! "Break opportunistic lock" command "on receive" event handler
      * \param cmd - Specified command
      * \param arg - arguments for the command
      * \param res - result of the command
      */
-    virtual void breakOplockSMBv2(const SMBv2::BreakOpLockCommand *, const SMBv2::BreakOpLockArgumentType &, const SMBv2::BreakOpLockResultType &) {}
+    virtual void breakOplockSMBv2(const SMBv2::BreakOpLockCommand*, const SMBv2::BreakOpLockArgumentType&, const SMBv2::BreakOpLockResultType&) {}
 };
 
 class IAnalyzer : public INFSv3rpcgen, public INFSv4rpcgen, public ISMBv1, public ISMBv2
