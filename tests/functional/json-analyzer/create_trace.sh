33e3bd225f60dfd8d6058f7bb12d36b1ed90b65d
Ilya V. Storozhilov <Ilya_Storozhilov@epam.com>
2014-12-12 15:20:45 +0300

    JSON analyzer dependencies have been added to CPack configuration

--- tests/functional/json-analyzer/create_trace.sh
+++ tests/functional/json-analyzer/create_trace.sh
@@ -25,6 +25,7 @@ if [ "$#" -lt 2 ] ; then
 fi
 
 NFS_DIR=$1
+TARGET_DIR="$NFS_DIR/nfstrace"
 FILES_AMOUNT=5
 FILES_SIZE="20K"
 SLEEP_DELAY="0.1"
@@ -32,13 +33,27 @@ SLEEP_DELAY="0.1"
 # Starting tcpdump
 CURRENT_USER=$(whoami)
 sudo tcpdump -i eth0 -w "$2" -Z $CURRENT_USER "port 2049" &
-TCPDUMP_PID=$!
+#sudo netsniff-ng --in eth0 --out "$2" --silent --filter "port 2049" &
+SNIFFER_PID=$!
 sleep 0.5
 
+mkdir $TARGET_DIR
+sleep $SLEEP_DELAY
 # Making FS-operations
 for file_no in $(seq 1 $FILES_AMOUNT) ; do
-    REMOTE_FILE="$NFS_DIR/test_file_$file_no"
+    REMOTE_FILE="$TARGET_DIR/test_file_$file_no"
     dd if=/dev/urandom of=$REMOTE_FILE bs=${FILES_SIZE} count=1
+    sleep $SLEEP_DELAY
+    chmod a+w $REMOTE_FILE
+    sleep $SLEEP_DELAY
+    REMOTE_FILE_HARD_LINK_NAME="$REMOTE_FILE.hard_link"
+    ln $REMOTE_FILE $REMOTE_FILE_HARD_LINK_NAME
+    sleep $SLEEP_DELAY
+    REMOTE_FILE_SOFT_LINK_NAME="$REMOTE_FILE.soft_link"
+    ln -s $REMOTE_FILE $REMOTE_FILE_SOFT_LINK_NAME
+    sleep $SLEEP_DELAY
+    REMOTE_FILE_SOFT_LINK_RENAMED="$REMOTE_FILE.soft_link.renamed"
+    mv $REMOTE_FILE_SOFT_LINK_NAME $REMOTE_FILE_SOFT_LINK_RENAMED
     sleep $SLEEP_DELAY
     LOCAL_FILE=$(mktemp)
     cp -v $REMOTE_FILE $LOCAL_FILE
@@ -46,14 +61,18 @@ for file_no in $(seq 1 $FILES_AMOUNT) ; do
     rm -f $LOCAL_FILE
     sleep $SLEEP_DELAY
 done
-ls -lh $NFS_DIR/
+df -h
 sleep $SLEEP_DELAY
-rm -fv $NFS_DIR/*
+ls -lh $TARGET_DIR/
+sleep $SLEEP_DELAY
+rm -fv $TARGET_DIR/*
+sleep $SLEEP_DELAY
+rmdir $TARGET_DIR
 sleep 0.5
 
 # Stopping tcpdump
-sudo kill $TCPDUMP_PID
-wait $TCPDUMP_PID
+sudo kill $SNIFFER_PID
+wait $SNIFFER_PID
 
 # Compressing trace file
 bzip2 -f "$2"
