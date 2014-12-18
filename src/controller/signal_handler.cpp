1bda22ce7d066540963a4484097c116a4ef8297e
Ilya V. Storozhilov <Ilya_Storozhilov@epam.com>
2014-12-18 17:10:16 +0300

    SIGINT handling code has been moved from controller::SignalHandler to controller::Controller

--- src/controller/signal_handler.cpp
+++ src/controller/signal_handler.cpp
@@ -69,10 +69,6 @@ static void handle_signals(const sigset_t    waitmask,
                                               "error in SignalHandler wait"});
             }
         }
-        else if(signo == SIGINT)
-        {
-            status.push(ProcessingDone{"Interrupted by user."});
-        }
         else
         {
             status.push(SignalHandler::Signal{signo});
