7f6f63d4f864c0ff1dbec97c9ef8c08398f2a5fd
Andrey Kuznetsov <Andrey_Kuznetsov@epam.com>
2014-12-26 15:11:15 +0300

    Wrong signatures in JSON plugin has been fixed.

--- analyzers/src/json/json_analyzer.h
+++ analyzers/src/json/json_analyzer.h
@@ -66,99 +66,99 @@ public:
 
     JsonAnalyzer(std::size_t workersAmount, int port, const std::string& host, std::size_t maxServingDurationMs, int backlog);
 
-    void null(const struct RPCProcedure* /*proc*/,
+    void null(const RPCProcedure* /*proc*/,
               const struct rpcgen::NULL3args* /*args*/,
               const struct rpcgen::NULL3res* /*res*/) override final;
 
-    void getattr3(const struct RPCProcedure* /*proc*/,
+    void getattr3(const RPCProcedure* /*proc*/,
                   const struct rpcgen::GETATTR3args* /*args*/,
                   const struct rpcgen::GETATTR3res* /*res*/) override final;
 
-    void setattr3(const struct RPCProcedure* /*proc*/,
+    void setattr3(const RPCProcedure* /*proc*/,
                   const struct rpcgen::SETATTR3args* /*args*/,
                   const struct rpcgen::SETATTR3res* /*res*/) override final;
 
-    void lookup3(const struct RPCProcedure* /*proc*/,
+    void lookup3(const RPCProcedure* /*proc*/,
                  const struct rpcgen::LOOKUP3args* /*args*/,
                  const struct rpcgen::LOOKUP3res* /*res*/) override final;
 
-    void access3(const struct RPCProcedure* /*proc*/,
+    void access3(const RPCProcedure* /*proc*/,
                  const struct rpcgen::ACCESS3args* /*args*/,
                  const struct rpcgen::ACCESS3res* /*res*/) override final;
 
-    void readlink3(const struct RPCProcedure* /*proc*/,
+    void readlink3(const RPCProcedure* /*proc*/,
                    const struct rpcgen::READLINK3args* /*args*/,
                    const struct rpcgen::READLINK3res* /*res*/) override final;
 
-    void read3(const struct RPCProcedure* /*proc*/,
+    void read3(const RPCProcedure* /*proc*/,
                const struct rpcgen::READ3args* /*args*/,
                const struct rpcgen::READ3res* /*res*/) override final;
 
-    void write3(const struct RPCProcedure* /*proc*/,
+    void write3(const RPCProcedure* /*proc*/,
                 const struct rpcgen::WRITE3args* /*args*/,
                 const struct rpcgen::WRITE3res* /*res*/) override final;
 
-    void create3(const struct RPCProcedure* /*proc*/,
+    void create3(const RPCProcedure* /*proc*/,
                  const struct rpcgen::CREATE3args* /*args*/,
                  const struct rpcgen::CREATE3res* /*res*/) override final;
 
-    void mkdir3(const struct RPCProcedure* /*proc*/,
+    void mkdir3(const RPCProcedure* /*proc*/,
                 const struct rpcgen::MKDIR3args* /*args*/,
                 const struct rpcgen::MKDIR3res* /*res*/) override final;
 
-    void symlink3(const struct RPCProcedure* /*proc*/,
+    void symlink3(const RPCProcedure* /*proc*/,
                   const struct rpcgen::SYMLINK3args* /*args*/,
                   const struct rpcgen::SYMLINK3res* /*res*/) override final;
 
-    void mknod3(const struct RPCProcedure* /*proc*/,
+    void mknod3(const RPCProcedure* /*proc*/,
                 const struct rpcgen::MKNOD3args* /*args*/,
                 const struct rpcgen::MKNOD3res* /*res*/) override final;
 
-    void remove3(const struct RPCProcedure* /*proc*/,
+    void remove3(const RPCProcedure* /*proc*/,
                  const struct rpcgen::REMOVE3args* /*args*/,
                  const struct rpcgen::REMOVE3res* /*res*/) override final;
 
-    void rmdir3(const struct RPCProcedure* /*proc*/,
+    void rmdir3(const RPCProcedure* /*proc*/,
                 const struct rpcgen::RMDIR3args* /*args*/,
                 const struct rpcgen::RMDIR3res* /*res*/) override final;
 
-    void rename3(const struct RPCProcedure* /*proc*/,
+    void rename3(const RPCProcedure* /*proc*/,
                  const struct rpcgen::RENAME3args* /*args*/,
                  const struct rpcgen::RENAME3res* /*res*/) override final;
 
-    void link3(const struct RPCProcedure* /*proc*/,
+    void link3(const RPCProcedure* /*proc*/,
                const struct rpcgen::LINK3args* /*args*/,
                const struct rpcgen::LINK3res* /*res*/) override final;
 
-    void readdir3(const struct RPCProcedure* /*proc*/,
+    void readdir3(const RPCProcedure* /*proc*/,
                   const struct rpcgen::READDIR3args* /*args*/,
                   const struct rpcgen::READDIR3res* /*res*/) override final;
 
-    void readdirplus3(const struct RPCProcedure* /*proc*/,
+    void readdirplus3(const RPCProcedure* /*proc*/,
                       const struct rpcgen::READDIRPLUS3args* /*args*/,
                       const struct rpcgen::READDIRPLUS3res* /*res*/) override final;
 
-    void fsstat3(const struct RPCProcedure* /*proc*/,
+    void fsstat3(const RPCProcedure* /*proc*/,
                  const struct rpcgen::FSSTAT3args* /*args*/,
                  const struct rpcgen::FSSTAT3res* /*res*/) override final;
 
-    void fsinfo3(const struct RPCProcedure* /*proc*/,
+    void fsinfo3(const RPCProcedure* /*proc*/,
                  const struct rpcgen::FSINFO3args* /*args*/,
                  const struct rpcgen::FSINFO3res* /*res*/) override final;
 
-    void pathconf3(const struct RPCProcedure* /*proc*/,
+    void pathconf3(const RPCProcedure* /*proc*/,
                    const struct rpcgen::PATHCONF3args* /*args*/,
                    const struct rpcgen::PATHCONF3res* /*res*/) override final;
 
-    void commit3(const struct RPCProcedure* /*proc*/,
+    void commit3(const RPCProcedure* /*proc*/,
                  const struct rpcgen::COMMIT3args* /*args*/,
                  const struct rpcgen::COMMIT3res* /*res*/) override final;
 
-    void null(const struct RPCProcedure* /*proc*/,
+    void null(const RPCProcedure* /*proc*/,
               const struct rpcgen::NULL4args* /*args*/,
               const struct rpcgen::NULL4res* /*res*/) override final;
 
-    void compound4(const struct RPCProcedure* /*proc*/,
+    void compound4(const RPCProcedure* /*proc*/,
                    const struct rpcgen::COMPOUND4args* /*args*/,
                    const struct rpcgen::COMPOUND4res* /*res*/) override final;
 
