f6ecf14b6db60f219cbd3c1fce5534df965394cf
Alexey Costroma <Alexey_Costroma@epam.com>
2015-01-16 13:41:05 +0300

    Code reorganizing

--- src/analysis/nfs_parser_thread.h
+++ src/analysis/nfs_parser_thread.h
@@ -55,19 +55,6 @@ private:
                                FilteredDataQueue::Ptr&& reply,
                                RPCSession* session);
 
-
-    //! Get NFSv4.x minor version
-    /*! This is a fast method. It doesn't call expensive XDR's mechanisms &
-    * doesn't create new objects. It simply moves pointer to a proper 
-    * place.
-    *
-    * According to NFSv4.0 & 4.1 RFC's it's possible to determine
-    * minor version ONLY in call COMPOUND(1) procedure.
-    * That's why only call can be passed here.
-    */
-
-    std::uint32_t get_nfs4_compound_minor_version(const std::uint8_t* rpc_nfs4_call);
-
     RunningStatus& status;
     Analyzers& analyzers;
     FilteredDataQueue& queue;
