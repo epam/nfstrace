fe22db17a1af391198886745b9194eba2d052149
Alexey Costroma <Alexey_Costroma@epam.com>
2015-01-16 13:41:05 +0300

    Code reorganizing

--- src/analysis/nfs_parser_thread.cpp
+++ src/analysis/nfs_parser_thread.cpp
@@ -142,6 +142,8 @@ void NFSParserThread::parse_data(FilteredDataQueue::Ptr&& ptr)
     }
 }
 
+static uint32_t get_nfs4_compound_minor_version(const std::uint8_t* rpc_nfs4_call);
+
 void NFSParserThread::analyze_nfs_operation( FilteredDataQueue::Ptr&& call,
                                              FilteredDataQueue::Ptr&& reply,
                                              RPCSession* session)
@@ -234,7 +236,16 @@ void NFSParserThread::analyze_nfs_operation( FilteredDataQueue::Ptr&& call,
     }
 }
 
-uint32_t NFSParserThread::get_nfs4_compound_minor_version(const std::uint8_t* rpc_nfs4_call)
+//! Get NFSv4.x minor version
+/*! This is a fast method. It doesn't call expensive XDR's mechanisms &
+* doesn't create new objects. It simply moves pointer to a proper 
+* place.
+*
+* According to NFSv4.0 & 4.1 RFC's it's possible to determine
+* minor version ONLY in call COMPOUND(1) procedure.
+* That's why only call can be passed here.
+*/
+static uint32_t get_nfs4_compound_minor_version(const std::uint8_t* rpc_nfs4_call)
 {
     // get initial data
     auto* it = rpc_nfs4_call;
