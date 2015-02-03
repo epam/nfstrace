7d88346618780926b3c684f26dc71bf8288f513c
Andrey Kuznetsov <Andrey_Kuznetsov@epam.com>
2015-02-03 17:50:16 +0300

    Dynamic load: dlsym functions have been incapsulated into cpp

--- src/utils/dynamic_load.h
+++ src/utils/dynamic_load.h
@@ -23,8 +23,6 @@
 #define DYNAMIC_LOAD_H
 //------------------------------------------------------------------------------
 #include <stdexcept>
-
-#include <dlfcn.h>
 //------------------------------------------------------------------------------
 namespace NST
 {
@@ -50,14 +48,19 @@ protected:
         static_assert(sizeof(void*) == sizeof(SymbolPtr), "object pointer and function pointer sizes must be equal");
 
         // suppression warning: ISO C++ forbids casting between pointer-to-function and pointer-to-object
-        using hook_dlsym_t = SymbolPtr (*)(void *, const char *);
+        using hook_dlsym_t = SymbolPtr (DynamicLoad::*)(const std::string&);
 
-        address = reinterpret_cast<hook_dlsym_t>(dlsym)(handle, name.c_str());
-        if(address == nullptr)
-        {
-            throw DLException{std::string{"Loading symbol "} + name + " failed with error:" + dlerror()};
-        }
-    }
+        hook_dlsym_t get_symbol_func = reinterpret_cast<hook_dlsym_t>(&DynamicLoad::get_symbol);
+        address = (*this.*get_symbol_func)(name);
+     }
+
+    /*!
+     * Gets symbol by name from DLL
+     * Throws exception if fails
+     * \param name - name of symbol
+     * \return pointer to valid symbol
+     */
+    void* get_symbol(const std::string& name);
 
 private:
     void* handle;
