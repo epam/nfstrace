77cafbf0b3482831436382a64a96f128ee3c2a43
Vitali_Adamenka <Vitali_Adamenka@epam.com>
2015-03-13 10:55:53 +0300

    Reorder headers. Code style fixes.

--- src/protocols/cifs2/cifs2.cpp
+++ src/protocols/cifs2/cifs2.cpp
@@ -19,8 +19,9 @@
     along with Nfstrace.  If not, see <http://www.gnu.org/licenses/>.
 */
 //------------------------------------------------------------------------------
-#include <arpa/inet.h>
 #include <cassert>
+
+#include <arpa/inet.h>
 
 #include "protocols/cifs2/cifs2.h"
 #include "protocols/cifs/cifs.h"
